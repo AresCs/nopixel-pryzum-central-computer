a7de4d42aa323e547776a361eb40ee110b8c5344
Kyle Peden <Kylepeden@hotmail.co.uk>
2024-08-05 17:34:49 +0100

    Updated login screen

--- src/Component/Login/Login.tsx
+++ src/Component/Login/Login.tsx
@@ -1,5 +1,6 @@
-// src/Component/Login/Login.tsx
 import React, { useState } from 'react';
+import './Login.css';
+import Logo from '../Logos/Pryzym_Logo2.png';
 
 interface LoginProps {
   onLogin: (username: string, password: string) => void;
@@ -15,31 +16,36 @@ const Login: React.FC<LoginProps> = ({ onLogin }) => {
   };
 
   return (
-    <div className="login-container">
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+    <div className="login-wrapper">
+      <div className="login-container">
+      <img src={Logo} className="app-logo" alt="Pryzum Logo" />
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit" className="login-button">Login</button>
+        </form>
+      </div>
+      <div className="noise"></div>
+      <div className="lines"></div>
     </div>
   );
 };
