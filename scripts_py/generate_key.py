a477486fb914313267bba360ac2d88a451c27b1e
Kyle Peden <Kylepeden@hotmail.co.uk>
2024-08-09 00:23:46 +0100

    Update generate key

--- scripts_py/generate_key.py
+++ scripts_py/generate_key.py
@@ -1,14 +1,18 @@
 import os
 from cryptography.fernet import Fernet
 
-def generate_and_save_key(key_file_path='../src/secure/key.key'):
-    # Ensure the directory exists
-    os.makedirs(os.path.dirname(key_file_path), exist_ok=True)
-    
-    key = Fernet.generate_key()
-    with open(key_file_path, 'wb') as key_file:
-        key_file.write(key)
-    print(f'Key generated and saved to {key_file_path}')
+# Get the absolute path to the project root
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
-if __name__ == "__main__":
-    generate_and_save_key()
+# Path to save the key file
+key_file_path = os.path.join(project_root, 'src/secure/key.key')
+
+# Ensure the output directory exists
+os.makedirs(os.path.dirname(key_file_path), exist_ok=True)
+
+# Generate the key and save it
+key = Fernet.generate_key()
+with open(key_file_path, 'wb') as key_file:
+    key_file.write(key)
+
+print(f'Key generated and saved to {key_file_path}')
