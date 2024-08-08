5fccb8f509454e0fc834f86f048fcc334e576056
Kyle Peden <Kylepeden@hotmail.co.uk>
2024-08-08 21:52:09 +0100

    Updating key file

--- scripts_py/generate_key.py
+++ scripts_py/generate_key.py
@@ -1,6 +1,10 @@
+import os
 from cryptography.fernet import Fernet
 
 def generate_and_save_key(key_file_path='../src/secure/key.key'):
+    # Ensure the directory exists
+    os.makedirs(os.path.dirname(key_file_path), exist_ok=True)
+    
     key = Fernet.generate_key()
     with open(key_file_path, 'wb') as key_file:
         key_file.write(key)
